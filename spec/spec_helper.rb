9e255caf1bebf6dc4d7a8b49c4fcb8290246abcc
myobie <me@nathanherald.com>
2015-08-18 13:23:13 +0200

    Use localhost redis for specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,7 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 ENV["RELIABLE_TIMEOUT"] = "1"
 ENV["RELIABLE_TIME_TRAVEL_DELAY"] = "1"
+ENV["REDIS_URI"] = "redis://127.0.0.1:6379/0"
 require 'reliable'
 
 RSpec.configure do |config|
